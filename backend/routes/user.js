faca8b41b5a35755d066c291d03a5fe6cedd59e5
marcelopetroni <marceloapetroni@hotmail.com>
2024-12-16 21:09:15 -0300

    fix: backend ajustments

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,19 @@
 import { UserController } from '../controllers/index.js';
-import { UserSquema } from '../schemas/index.js';
-import { SchemaValidator } from '../middlewares/SchemaValidator.js';
+import { UserSchema } from '../schemas/index.js';
+import { SchemaValidator } from '../middlewares/index.js';
+import { Router } from 'express';
 
 export default class userRoutes {
 	constructor() {
+        this.router = new Router();
 		this.UserController = new UserController();
         this.SchemaValidator = SchemaValidator;
 	}
 
 	setup() {
-		this.router.post('/', this.SchemaValidator.validate(UserSquema.create), this.UserController.create);
-		this.router.get('/:id/exists', this.SchemaValidator.validate(UserSquema.exists), this.UserController.exists);
-		this.router.put('/:id', this.SchemaValidator.validate(UserSquema.update), this.UserController.update);
+		this.router.post('/', this.SchemaValidator.validate(UserSchema.create), this.UserController.create);
+		this.router.get('/:id/exists', this.SchemaValidator.validate(UserSchema.exists), this.UserController.exists);
+		this.router.put('/:id', this.SchemaValidator.validate(UserSchema.update), this.UserController.update);
 
 		return this.router;
 	}
