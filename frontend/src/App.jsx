7390510daca6f717624e962562ae7273b96ce754
Breno do Amaral <brenodoamaral1@gmail.com>
2024-12-16 23:56:48 -0300

    Home Pagem correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './views/LandingPage'
-import Overview from './views/Overview';
+import Home from './views/Home';
 import Certificates from './views/Certificates';
 import Achievements from './views/Achievements/Achievements';
 import Wallet from './views/Wallet/Wallet';
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/overview" element={<Overview />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/wallet" element={<Wallet />} />
         <Route path="/certificates" element={<Certificates />} />
         <Route path="/achievements" element={<Achievements />} />
