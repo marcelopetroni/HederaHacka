83daa5baa63e9fac174c2769b1fdc21acd5606e7
marcelopetroni <marceloapetroni@hotmail.com>
2024-12-18 16:22:29 -0300

    chore: identantion ajustment

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'
+	dialect: 'postgres'
 });
 
 export { sequelize };
